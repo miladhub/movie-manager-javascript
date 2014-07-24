e74b1cde255f12720ee02f3100c744a0761a5f5b
milad <milad.bourhani@i4canalytics.com>
2014-07-24 23:01:10 +0200

    reused presenter and interactor from websocket endpoint

--- movie-manager-web/src/main/java/movie/MovieEndpoint.java
+++ movie-manager-web/src/main/java/movie/MovieEndpoint.java
@@ -1,16 +1,54 @@
 package movie;
 
 import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.websocket.EncodeException;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import movie.j2ee.interfaces.MovieRepository;
+
 @ServerEndpoint("/ws")
-public class MovieEndpoint {
-    @OnMessage
-    public void message(String message, Session client) throws IOException, EncodeException {
-        System.out.println("message: " + message + ", found " + (client.getOpenSessions().size() - 1) + " other sessions");
-        client.getBasicRemote().sendText(message);
-    }
+public class MovieEndpoint implements MovieView {
+	@Inject
+	private MovieRepository model;
+	private MoviePresenter presenter;
+	private Session session;
+
+	@PostConstruct
+	public void createPresenter() {
+		presenter = new MoviePresenter(this, model);
+	}
+
+	@OnOpen
+	public void open(Session session) {
+		this.session = session;
+	}
+
+	@OnMessage
+	public void search(String query) throws IOException, EncodeException {
+		presenter.search(query);
+	}
+
+	@Override
+	public void displayMatches(String matches) {
+		try {
+			session.getBasicRemote().sendText(matches);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void displayErrorMessage(String error, String detail) {
+		try {
+			session.getBasicRemote().sendText(error + " (" + detail + ")");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
